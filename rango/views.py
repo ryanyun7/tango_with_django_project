1d7451d913fb5646f89729020867a89f6c564ea1
Jinsik Yun <ryanyun7@gmail.com>
2020-02-15 12:51:11 +0000

    chaper6 complete

--- rango/views.py
+++ rango/views.py
@@ -11,10 +11,11 @@ def index(request) :
 	# Place the list in our context_dict dictionary (with our boldmessage!)
 	# that will be passed to the template engine.
 	category_list = Category.objects.order_by('-likes') [:5]
+	page_list = Page.objects.order_by('views') [:5]
 
-	context_dict = {}
+	context_dict = {'categories':category_list, 'pages' : page_list}
 	context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
-	context_dict['categories'] = category_list
+
 
 
 	# Render the response and send it back!
